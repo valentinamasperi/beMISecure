d497d70cb91b00a45b8d944778034843c4d923bf
valentina <fpesce@hotelcube.eu>
2017-01-14 16:25:12 +0000

    ciao mario xd

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204170247) do
+ActiveRecord::Schema.define(version: 20170114160700) do
+
+  create_table "stations", force: :cascade do |t|
+    t.integer  "position"
+    t.integer  "number_chains"
+    t.integer  "station_number"
+    t.integer  "free_chains"
+    t.string   "station_name"
+    t.integer  "lockers"
+    t.integer  "security"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
