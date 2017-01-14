d3473eb6eff708cd299414583920c97c74b86a1d
teresavalbonesi <teresavalbonesi@gmail.com>
2017-01-14 16:30:19 +0000

    il mio modello user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204170247) do
+ActiveRecord::Schema.define(version: 20170114161144) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "surname"
+    t.date     "birthdate"
+    t.string   "phonenumber"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
