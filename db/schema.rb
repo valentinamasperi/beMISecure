1e976b61831cfb40000a9d3ca98618961eb72205
Mario Polito <mariopolito69@gmail.com>
2017-01-17 13:31:19 +0000

    commit provvisorio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204170247) do
+ActiveRecord::Schema.define(version: 20161217163155) do
+
+  create_table "geografia", force: :cascade do |t|
+    t.string   "name"
+    t.string   "posizione"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
